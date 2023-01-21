e92d418eb4627eac00f400f80f954af722339a38
Dani Setien <dani.setien@gmail.com>
2023-01-21 15:55:57 +0100

    Correct test indentation

--- js/findCartesianCoordinates/findCartesianCoordinates.test.js
+++ js/findCartesianCoordinates/findCartesianCoordinates.test.js
@@ -1,10 +1,7 @@
 import findCartesianCoordinates from "./findCartesianCoordinates.js";
 
 describe("Given a findCartesianCoordinates function", () => {
-  describe(`When it receives the position 2 and an unarranged board in list form 
-  [0,1,2,
-   3,4,5,
-   6,7,8]`, () => {
+  describe("When it receives the position 2 and an unarranged board in list form [0,1,2,3,4,5,6,7,8]", () => {
     test("Then it should return its [x,y] coordinates as [1,1]", () => {
       const givenUnarrangedBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8];
       const givenPosition = 2;
@@ -19,12 +16,7 @@ describe("Given a findCartesianCoordinates function", () => {
     });
   });
 
-  describe(`When it receives the position 0 and an unarranged board in list form 
-  [00,01,02,03,04,
-   05,06,07,08,09,
-   10,11,12,13,14,
-   15,16,17,18,18,
-   20,21,22,23,24]`, () => {
+  describe("When it receives the position 0 and an unarranged board in list form [00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,18,20,21,22,23,24]", () => {
     test("Then it should return its [x,y] coordinates as [0,0]", () => {
       const givenUnarrangedBoard = [
         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 18,
@@ -42,12 +34,7 @@ describe("Given a findCartesianCoordinates function", () => {
     });
   });
 
-  describe(`When it receives the position 24 and an unarranged board in list form 
-  [00,01,02,03,04,
-   05,06,07,08,09,
-   10,11,12,13,14,
-   15,16,17,18,18,
-   20,21,22,23,24]`, () => {
+  describe("When it receives the position 24 and an unarranged board in list form [00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,18,20,21,22,23,24]", () => {
     test("Then it should return its [x,y] coordinates as [4,4]", () => {
       const givenUnarrangedBoard = [
         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 18,
